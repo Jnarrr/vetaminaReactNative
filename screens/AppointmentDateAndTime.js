c08cff139ee54a53ac28f09597c208b0fe67f7e7
Jnarrr <lfebruary60@gmail.com>
2022-10-08 02:21:46 +0800

    combobox& radio button

--- screens/AppointmentDateAndTime.js
+++ screens/AppointmentDateAndTime.js
@@ -5,6 +5,11 @@ const AppointmentDateAndTimeScreen = ( {navigation} ) => {
     return(
         <View style = {{ flex: 1, justifyContent: 'center', justifyContent: 'center', alignItems: 'center'}}>
             <Text style = {{ fontSize: 20, color: 'black'}}>Appointment Date And Time View</Text>
+            {/*<RadioButton
+                value="first"
+                status={ checked === 'first' ? 'checked' : 'unchecked' }
+                onPress={() => setChecked('first')}
+    />*/}
             <Button onPress={ () => navigation.navigate('AppointmentSelectPet')} title='Next'></Button>
         </View>
     );
--- screens/AppointmentProcedure.js
+++ screens/AppointmentProcedure.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import {View, Button, Text} from 'react-native';
+import React, { useState } from 'react';
+import {View, Button, Text, StyleSheet} from 'react-native';
+import CheckBox from '@react-native-community/checkbox';
 
 const AppointmentProcedureScreen = ( {navigation} ) => {
+    const [isSelected, setSelection] = useState(false);
+
     return(
         <View style = {{ flex: 1, justifyContent: 'center', justifyContent: 'center', alignItems: 'center'}}>
             <Text style = {{ fontSize: 20, color: 'black'}}>Appointment Procedure View</Text>
+            <View>
+                <CheckBox
+                value={isSelected}
+                onValueChange={setSelection}
+                style={styles.checkbox}
+                />
+                <Text style={styles.label}>Do you like React Native?</Text>
+            </View>
+            <Text>Is CheckBox selected: {isSelected ? "👍" : "👎"}</Text>
             <Button onPress={ () => navigation.navigate('AppointmentDateAndTime')} title='Next'></Button>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    checkboxContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+    },
+    checkbox: {
+    alignSelf: "center",
+    onCheckColor: '#6DA916',
+    onFillColor: '#6DA916',
+    onTintColor: '#6DA916',
+    },
+    label: {
+    margin: 8,
+    color: 'black',
+    },
+})
 
 export default AppointmentProcedureScreen;
